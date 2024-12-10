94050d0077e90c7c7e25e53c8ec4757df6192e99
Elif İrem Kara Kadyrov <83959541+elfrmkr@users.noreply.github.com>
2024-12-10 10:10:44 +0100

    feat: added visuals; permission to access internet to get the data

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,9 +51,16 @@ android {
 
 dependencies {
 
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
+    //Compose ViewModel
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.7")
+
+    //Network calls
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
+
+    //Json to Kotlin object mapping
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+
+    //Image loading
     implementation("io.coil-kt:coil-compose:2.4.0")
 
     implementation("androidx.core:core-ktx:1.15.0")
@@ -63,7 +70,7 @@ dependencies {
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.material3:material3")
+    implementation("androidx.compose.material3:material3:1.3.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
